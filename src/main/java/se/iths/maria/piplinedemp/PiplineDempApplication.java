2668295fe909c4fda4751a512d6bbb1021d28aa8
Mariapalm98 <mariapalma.mpp@gmail.com>
2026-02-12 10:22:06 +0100

    initial commit

---
+++ src/main/java/se/iths/maria/piplinedemp/PiplineDempApplication.java
@@ -0,0 +1,13 @@
+package se.iths.maria.piplinedemp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PiplineDempApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PiplineDempApplication.class, args);
+    }
+
+}
---
+++ src/test/java/se/iths/maria/piplinedemp/PiplineDempApplicationTests.java
@@ -0,0 +1,13 @@
+package se.iths.maria.piplinedemp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PiplineDempApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
